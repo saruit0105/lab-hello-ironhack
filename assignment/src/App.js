72549d02125eddcd30d22013e18d891214aa2620
saruit0105 <saruits2@gmail.com>
2019-11-11 20:57:56 -0500

    worked on code

---
+++ assignment/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import icon1 from "images/icon1.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Say hello to ReactJs </h1>
+        <p>You will learn a frontend framework from scratch to become a Ninka Developer</p>
+        <button>Awesome!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
